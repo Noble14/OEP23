db53367787e3d3a372deeafa9e7a26e0e48258ec
Noble14 <nemesz2000@gmail.com>
2023-03-22 21:43:20 +0100

    create rational solution

---
+++ rational/rational/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace rational
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            try
+            {
+                Rational r1 = new Rational(1, 2);
+                Rational r2 = new Rational(3, 4);
+
+                //r1.Add(r2);
+                //Rational.Add(r1, r2);
+                Rational res = r1 + r2;
+                Console.WriteLine(res);
+            }
+            catch (Rational.ZeroDivisionException)
+            {
+
+                Console.WriteLine("Nulla az osztóban");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ rational/rational/rational.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace rational
+{
+    class Rational
+    {
+        private int n, d;
+
+        public class ZeroDivisionException : Exception { }
+
+        public Rational(int n, int d)
+        {
+            if (d == 0) throw new ZeroDivisionException();
+            this.d = d;
+            this.n = n;
+            norm();
+        }
+
+        public override string ToString()
+        {
+            return n + "/" + d;
+        }
+
+        private void norm ()
+        {
+            int greater = Math.Max(n, d);
+            int lower = Math.Min(n, d);
+            int m = greater % lower;
+            while (m != 0)
+            {
+                greater = lower;
+                lower = m;
+                m = greater % lower;
+            }
+            d = d / lower;
+            n = n / lower;
+        }
+
+        public static Rational operator +(Rational r1, Rational r2)
+        {
+            return new Rational(r1.n * r2.d + r2.n * r1.d, r1.d * r2.d);
+        }
+        public static Rational operator -(Rational r1, Rational r2)
+        {
+            return new Rational(r1.n * r2.d - r2.n * r1.d, r1.d * r2.d);
+        }
+        public static Rational operator * (Rational r1, Rational r2)
+        {
+            return new Rational(r1.n * r2.n,r1.d * r2.d);
+        }
+        public static Rational operator /(Rational r1, Rational r2)
+        {
+            if (r2.n == 0) throw new ZeroDivisionException();
+            return new Rational(r1.n * r2.d, r1.d * r2.n);
+        }
+    }
+}
