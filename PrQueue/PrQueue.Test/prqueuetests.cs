6daaf04181e335033c9432740ce025d4a92750f3
Zoltán Nemes <p4pc4k@INF.ELTE.HU>
2023-03-23 13:41:22 +0100

    gyak3 more tests

--- PrQueue/PrQueue.Test/prqueuetests.cs
+++ PrQueue/PrQueue.Test/prqueuetests.cs
@@ -22,6 +22,16 @@ namespace PrQueue.Test
         [TestMethod]
         public void RemMaxTest()
         {
+            PrQueue p = new();
+            Assert.ThrowsException<PrQueue.NoElementsExeption>(() => p.RemMax());
+            Assert.ThrowsException<PrQueue.NoElementsExeption>(() => p.GetMax());
+            
+            p.Add(new Element(1, "h1"));
+            p.Add(new Element(10, "h2"));
+            p.Add(new Element(12, "h2"));
+            p.Add(new Element(9, "h2"));
+            p.Add(new Element(8, "h2"));
+            p.Add(new Element(3, "h2"));
         }
         [TestMethod]
         public void GetMaxTest()
