5d9e5e8359795c2661393b58c34b747e324dc973
Zoltán Nemes <p4pc4k@INF.ELTE.HU>
2023-03-23 09:55:06 +0100

    add gyak3

---
+++ PrQueue/PrQueue.Test/prqueuetests.cs
@@ -0,0 +1,40 @@
+namespace PrQueue.Test
+{
+    [TestClass]
+    public class prqueuetests
+    {
+        [TestMethod]
+        public void AddTest()
+        {
+            PrQueue p = new PrQueue();
+            Assert.IsTrue(p.IsEmpty());
+            p.Add(new Element(10,"Hello"));
+            Assert.IsFalse(p.IsEmpty());
+            p.SetEmpty();
+            Assert.IsTrue(p.IsEmpty());
+            p.Add(new Element(10, "Hello"));
+            p.Add(new Element(10, "Hello"));
+            Assert.IsFalse(p.IsEmpty());
+            p.SetEmpty();
+            Assert.IsTrue(p.IsEmpty());
+
+        }
+        [TestMethod]
+        public void RemMaxTest()
+        {
+        }
+        [TestMethod]
+        public void GetMaxTest()
+        {
+            PrQueue p = new();
+            p.Add(new Element(1,"h1"));
+            p.Add(new Element(10,"h2"));
+            p.Add(new Element(12,"h2"));
+            p.Add(new Element(9,"h2"));
+            p.Add(new Element(8,"h2"));
+            p.Add(new Element(3,"h2"));
+
+            Assert.AreEqual(12, p.GetMax().Prioritas);
+        }
+    }
+}
---
+++ PrQueue/PrQueue/PrQueue.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrQueue
+{
+    public class Element
+    {
+        public int Prioritas;
+        public string Value;
+
+        public Element(int pr, string val)
+        {
+            Prioritas = pr;
+            Value = val;
+        }
+    }
+    public class PrQueue
+    {
+        List<Element> Elements = new ();
+        public class NoElementsExeption : Exception { }
+
+        public PrQueue()
+        {
+            
+        }
+
+        public void SetEmpty()
+        {
+            Elements.Clear();
+        }
+        public bool IsEmpty()
+        {
+            return Elements.Count == 0;
+        }
+        public void Add(Element e)
+        {
+            Elements.Add(e);
+        }
+        private int GetMaxIndex()
+        {
+            int maxPr = Elements[0].Prioritas;
+            int index = 0;
+            for (int i = 0; i < Elements.Count; i++)
+            {
+                if (maxPr < Elements[i].Prioritas)
+                {
+                    maxPr = Elements[i].Prioritas;
+                    index = i;
+                }
+            }
+            return index;
+        }
+        public Element GetMax()
+        {
+            if (Elements.Count == 0)
+                throw new NoElementsExeption();
+            return Elements[GetMaxIndex()];
+        }
+        public Element RemMax()
+        {
+            if (Elements.Count == 0)
+                throw new NoElementsExeption();
+            Element max = Elements[GetMaxIndex()];
+            Elements.RemoveAt(GetMaxIndex());
+            return max;
+            //return Elements[GetMaxIndex()];
+        }
+    }
+}
