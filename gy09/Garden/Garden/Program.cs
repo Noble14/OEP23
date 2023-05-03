0e634323a29cadc133148323f062ed8a3e95be57
Noble14 <nemesz2000@gmail.com>
2023-05-03 21:24:56 +0200

    pre main

---
+++ gy09/Garden/Garden/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace Garden
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Garden garden = new(5);
+            Gardener gardener = new(garden);
+
+            gardener.Plant(1, Potatoe.Instance(), 3);
+            gardener.Plant(2, Pea.Instance(), 3);
+            gardener.Plant(4, Pea.Instance(), 3);
+
+            Console.Write("A betakarithato parcellak azonositoi: ");
+            foreach (int i in gardener.garden.CanHarvest(6))
+            {
+                Console.Write($"{i} ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
